956df357caae87a4a7091ccd3c76664483c0ab9c
Emils Bernhards <emilsbernhards@Emilss-MacBook-Pro-7.local>
2019-05-04 17:31:54 +0200

    Added temperature high and low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,15 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. ' + "Today's high will be " + body.daily.data[0].temperatureHigh + ' and the low will be ' + body.daily.data[0].temperatureLow + ' degrees.')
+
         }
     })
 }
 
 module.exports = forecast
+
+//temperature high and temperature low of the day
+
+// body.daily.data.temperatureHigh
+// body.daily.data.temperatureLow
